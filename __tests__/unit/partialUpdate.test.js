eaaa0fb7fcaedb82363e8336c6b9f61aea87cd1b
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-07 10:48:48 -0700

    partialquery tests

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,18 @@
+process.env.NODE_ENV = "test";
+const request = require("supertest");
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+	it("should generate a proper partial update query with just 1 field", function () {
+		const items = {
+			fname: "updatedFirstName",
+			lname: "updatedLastName",
+		};
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+		const result = sqlForPartialUpdate("user", items, "id", "1");
+		const query = `UPDATE user SET fname=$1, lname=$2 WHERE id=$3 RETURNING *`;
+		const values = [items["fname"], items["lname"], "1"];
 
-  });
+		expect(result).toEqual({ query, values });
+	});
 });
