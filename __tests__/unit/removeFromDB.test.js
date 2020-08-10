788bb8bfa8e117409ba328615280ff6698b22c6a
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-10 09:03:29 -0700

    test for delete helper

---
+++ __tests__/unit/removeFromDB.test.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = "test";
+const request = require("supertest");
+const sqlForDelete = require("../../helpers/removeFromDB");
+
+describe("sqlForDelete()", () => {
+	test("it should create a query string with data to be deleted", async () => {
+		let query = sqlForDelete("companies", "name", "Apple");
+		let answer = { query: "DELETE FROM companies WHERE name=$1 RETURNING *", id: "Apple" };
+
+		expect(answer).toEqual(query);
+	});
+
+	test("it should throw an error if all three paramters are not provided", async () => {
+		let e;
+		try {
+			sqlForDelete("companies", "name");
+		} catch (error) {
+			e = error;
+		}
+
+		expect(e.message).toEqual("all parameters are required");
+		expect(e.status).toEqual(500);
+	});
+});
