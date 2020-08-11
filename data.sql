42b2685d2c3b235b54edeed98479daba398e3e30
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-11 09:43:34 -0700

    base user

--- data.sql
+++ data.sql
@@ -3,6 +3,7 @@
 
 DROP TABLE IF EXISTS companies CASCADE;
 DROP TABLE IF EXISTS jobs CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE companies (
     handle TEXT PRIMARY KEY,
@@ -20,4 +21,14 @@ CREATE TABLE jobs (
     company_handle TEXT NOT NULL REFERENCES companies ON DELETE CASCADE,
     date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT max_equity_check CHECK ((equity < 1))
+);
+
+CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL, 
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    photo_url TEXT,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
---
+++ models/userModel.js
@@ -0,0 +1,88 @@
+const db = require("../db");
+const ExpressError = require("../helpers/expressError");
+const sqlForPartialUpdate = require("../helpers/partialUpdate");
+const sqlForDelete = require("../helpers/removeFromDB");
+
+/**collection of related methods for user */
+
+class User {
+	constructor({ username, password, first_name, last_name, email, photo_url, is_admin }) {
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.photo_url = photo_url;
+		this.is_admin = is_admin;
+	}
+
+	/** creates a new user */
+	static async create({
+		username,
+		password,
+		first_name,
+		last_name,
+		email,
+		photo_url = "",
+		is_admin = false,
+	}) {
+		const result = await db.query(
+			`INSERT INTO jobs (username, password, first_name, last_name, email, photo_url, is_admin)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING username, password, first_name, last_name, email, photo_url, is_admin`,
+			[username, password, first_name, last_name, email, photo_url, is_admin]
+		);
+		const user = result.rows[0];
+
+		if (user === undefined) {
+			const err = new ExpressError("Could not create user", 400);
+			throw err;
+		}
+		return new User(user);
+	}
+	/** get all users */
+	static async all() {
+		const result = await db.query(
+			`SELECT username, password, first_name, last_name, email, photo_url, is_admin FROM users ORDER BY last_name, first_name`
+		);
+		return result.rows.map((u) => new User(u));
+	}
+	/** get user by username */
+	static async get(username) {
+		const result = await db.query(
+			`SELECT username, password, first_name, last_name, email, photo_url, is_admin FROM users WHERE username=$1`,
+			[username]
+		);
+		const user = result.rows[0];
+
+		if (user === undefined) {
+			const err = new ExpressError(`Could not find user id: ${id}`, 404);
+			throw err;
+		}
+		return new User(user);
+	}
+
+	/** update user
+	 * - items: an object with keys of columns you want to update and values with updated values
+	 */
+
+	async update(items) {
+		const updateData = sqlForPartialUpdate("users", items, "username", this.username);
+		const result = await db.query(updateData.query, updateData.values);
+		return result.rows[0];
+	}
+
+	/** remove user with matching id */
+	static async remove(username) {
+		let queryString = sqlForDelete("users", "username", username);
+		const result = await db.query(queryString.query, [queryString.username]);
+
+		if (result.rows.length === 0) {
+			const err = new ExpressError(`Could not find user username: ${username}`, 404);
+			throw err;
+		}
+		return "deleted";
+	}
+}
+
+module.exports = User;
