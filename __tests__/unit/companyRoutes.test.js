fcc99e6f9eb3c90d7f0113a5f2df9394f5cac9a8
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-11 16:55:13 -0700

    updated tests for auth

--- __tests__/unit/companyRoutes.test.js
+++ __tests__/unit/companyRoutes.test.js
@@ -4,11 +4,16 @@ const db = require("../../db");
 const app = require("../../app");
 const Company = require("../../models/companyModel");
 const Job = require("../../models/jobModel");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = require("../../config");
 console.error = jest.fn();
 
-describe("test company routes", () => {
+describe("test company routes", async () => {
 	let c;
 	let values;
+	// create token don't actually go into DB and add user
+	let adminUserToken = jwt.sign({ username: "testUser", is_admin: true }, JWT_SECRET_KEY);
+	let generalUserToken = jwt.sign({ username: "testUser", is_admin: false }, JWT_SECRET_KEY);
 	beforeEach(async function () {
 		await db.query("DELETE FROM jobs");
 		await db.query("DELETE FROM companies");
@@ -22,14 +27,39 @@ describe("test company routes", () => {
 		c = await Company.create(values);
 	});
 	describe("GET /companies", () => {
-		test("get all companies", async () => {
-			let resp = await request(app).get("/companies");
+		test("get all companies with logged in admin user", async () => {
+			let resp = await request(app).get("/companies").send({ _token: adminUserToken });
 			expect(resp.statusCode).toBe(200);
 			expect(resp.body).toEqual({ companies: [c] });
 		});
+		test("get all companies with logged in general user", async () => {
+			let resp = await request(app).get("/companies").send({ _token: generalUserToken });
+			expect(resp.statusCode).toBe(200);
+			expect(resp.body).toEqual({ companies: [c] });
+		});
+		test("return error if user is not logged in", async () => {
+			let resp = await request(app).get("/companies");
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
 	});
 	describe("POST /companies", () => {
-		test("creates a company", async () => {
+		test("creates a company with logged in admin user", async () => {
+			v = {
+				handle: "UNH",
+				name: "UnitedHealthGroup",
+				num_employees: 10,
+				description: "Health company",
+				logo_url:
+					"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+				_token: adminUserToken,
+			};
+			let resp = await request(app).post("/companies").send(v);
+			expect(resp.statusCode).toBe(201);
+			delete v._token;
+			expect(resp.body).toEqual({ company: v });
+		});
+		test("return error if user is not logged in", async () => {
 			v = {
 				handle: "UNH",
 				name: "UnitedHealthGroup",
@@ -39,8 +69,22 @@ describe("test company routes", () => {
 					"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
 			};
 			let resp = await request(app).post("/companies").send(v);
-			expect(resp.statusCode).toBe(201);
-			expect(resp.body).toEqual({ company: v });
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
+		test("return error if user is not an admin", async () => {
+			v = {
+				handle: "UNH",
+				name: "UnitedHealthGroup",
+				num_employees: 10,
+				description: "Health company",
+				logo_url:
+					"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+				_token: generalUserToken,
+			};
+			let resp = await request(app).post("/companies").send(v);
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
 		});
 		test("provides an error response if handle is not provided", async () => {
 			v = {
@@ -49,6 +93,7 @@ describe("test company routes", () => {
 				description: "Health company",
 				logo_url:
 					"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+				_token: adminUserToken,
 			};
 			let resp = await request(app).post("/companies").send(v);
 			expect(resp.body.status).toBe(400);
@@ -61,6 +106,7 @@ describe("test company routes", () => {
 				description: "Health company",
 				logo_url:
 					"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+				_token: adminUserToken,
 			};
 			let resp = await request(app).post("/companies").send(v);
 			expect(resp.body.status).toBe(400);
@@ -72,6 +118,7 @@ describe("test company routes", () => {
 				name: 1,
 				description: 1,
 				logo_url: 1,
+				_token: adminUserToken,
 			};
 			let resp = await request(app).post("/companies").send(v);
 			expect(resp.statusCode).toBe(400);
@@ -84,19 +131,53 @@ describe("test company routes", () => {
 		});
 	});
 	describe("GET /companies/:handle", () => {
-		test("get details on company when there is no job data", async () => {
-			let resp = await request(app).get(`/companies/${values.handle}`);
+		test("get details on company when there is no job data and admin is logged in", async () => {
+			let resp = await request(app)
+				.get(`/companies/${values.handle}`)
+				.send({ _token: adminUserToken });
 			expect(resp.statusCode).toBe(200);
 			expect(resp.body).toEqual({ company: c });
 		});
-		test("get details on company when it has job data", async () => {
+		test("get details on company when there is no job data and general user is logged in", async () => {
+			let resp = await request(app)
+				.get(`/companies/${values.handle}`)
+				.send({ _token: generalUserToken });
+			expect(resp.statusCode).toBe(200);
+			expect(resp.body).toEqual({ company: c });
+		});
+		test("get details on company when it has job data and admin is logged in", async () => {
 			let job = await Job.create({
 				title: "owner",
 				salary: 100000,
 				equity: 0.9,
 				company_handle: values.handle,
 			});
-			let resp = await request(app).get(`/companies/${values.handle}`);
+			let resp = await request(app)
+				.get(`/companies/${values.handle}`)
+				.send({ _token: adminUserToken });
+			c.jobs = [
+				{
+					title: job.title,
+					salary: job.salary,
+					equity: job.equity,
+					id: job.id,
+					date_posted: expect.any(String),
+				},
+			];
+
+			expect(resp.statusCode).toBe(200);
+			expect(resp.body).toEqual({ company: c });
+		});
+		test("get details on company when it has job data and general user is logged in", async () => {
+			let job = await Job.create({
+				title: "owner",
+				salary: 100000,
+				equity: 0.9,
+				company_handle: values.handle,
+			});
+			let resp = await request(app)
+				.get(`/companies/${values.handle}`)
+				.send({ _token: generalUserToken });
 			c.jobs = [
 				{
 					title: job.title,
@@ -111,18 +192,26 @@ describe("test company routes", () => {
 			expect(resp.body).toEqual({ company: c });
 		});
 		test("provides an error response if company can't be found", async () => {
-			let resp = await request(app).get(`/companies/invalid`);
+			let resp = await request(app)
+				.get(`/companies/invalid`)
+				.send({ _token: generalUserToken });
 			expect(resp.statusCode).toBe(404);
 			expect(resp.body.message).toEqual("Could not find company handle: invalid");
 		});
+		test("provides an error response if user is not logged in", async () => {
+			let resp = await request(app).get(`/companies/${values.handle}`);
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
 	});
 	describe("PATCH /companies/:handle", () => {
-		test("update company details", async () => {
+		test("update company details when admin is logged in", async () => {
 			update = {
 				name: "updatedName",
 				num_employees: 1234567,
 				description: "updatedDescription",
 				logo_url: "updatedURL",
+				_token: adminUserToken,
 			};
 
 			let resp = await request(app).patch(`/companies/${values.handle}`).send(update);
@@ -140,6 +229,7 @@ describe("test company routes", () => {
 				name: 1,
 				description: 1,
 				logo_url: 1,
+				_token: adminUserToken,
 			};
 			let resp = await request(app).patch(`/companies/${values.handle}`).send(updatedValues);
 			expect(resp.statusCode).toBe(400);
@@ -149,17 +239,60 @@ describe("test company routes", () => {
 				"instance.logo_url is not of a type(s) string",
 			]);
 		});
+		test("provides an error response if general user is logged in", async () => {
+			update = {
+				name: "updatedName",
+				num_employees: 1234567,
+				description: "updatedDescription",
+				logo_url: "updatedURL",
+				_token: generalUserToken,
+			};
+
+			let resp = await request(app).patch(`/companies/${values.handle}`).send(update);
+
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
+		test("provides an error response if no user is logged in", async () => {
+			update = {
+				name: "updatedName",
+				num_employees: 1234567,
+				description: "updatedDescription",
+				logo_url: "updatedURL",
+			};
+
+			let resp = await request(app).patch(`/companies/${values.handle}`).send(update);
+
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
 	});
 	describe("DELETE /companies/:handle", () => {
-		test("deletes a company based on the provided handle", async () => {
-			let resp = await request(app).delete(`/companies/${values.handle}`);
+		test("deletes a company based on the provided handle when an admin is logged in", async () => {
+			let resp = await request(app)
+				.delete(`/companies/${values.handle}`)
+				.send({ _token: adminUserToken });
 			expect(resp.statusCode).toBe(200);
 			expect(resp.body).toEqual({ message: "Company deleted" });
 		});
-		test("provides an error response if the company can not be found", async () => {
-			let resp = await request(app).delete(`/companies/invalid`);
+		test("provides an error response if the company can not be found when an admin is logged in", async () => {
+			let resp = await request(app)
+				.delete(`/companies/invalid`)
+				.send({ _token: adminUserToken });
 			expect(resp.statusCode).toBe(404);
 			expect(resp.body.message).toEqual(`Could not find company handle: invalid`);
+		});
+		test("provides an error response if general user is logged in", async () => {
+			let resp = await request(app)
+				.delete(`/companies/${values.handle}`)
+				.send({ _token: generalUserToken });
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
+		test("provides an error response if no user is logged in", async () => {
+			let resp = await request(app).delete(`/companies/${values.handle}`);
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
 		});
 	});
 });
