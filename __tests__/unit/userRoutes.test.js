d5377dea9b8baabd89d2069b36496a547fb52e20
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-11 17:50:17 -0700

    updated for auth

--- __tests__/unit/userRoutes.test.js
+++ __tests__/unit/userRoutes.test.js
@@ -3,12 +3,15 @@ const request = require("supertest");
 const db = require("../../db");
 const app = require("../../app");
 const User = require("../../models/userModel");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = require("../../config");
 console.error = jest.fn();
 
 describe("test user routes", () => {
 	let u; //will not show sensitive info
 	let uAll; // will show all user info
 	let values;
+	let uToken;
 	beforeEach(async function () {
 		await db.query("DELETE FROM users");
 		values = {
@@ -23,6 +26,7 @@ describe("test user routes", () => {
 		};
 		uAll = await User.create(values);
 		u = uAll;
+		uToken = jwt.sign({ username: u.username, is_admin: true }, JWT_SECRET_KEY);
 		delete u.password;
 		delete u.is_admin;
 	});
@@ -48,7 +52,8 @@ describe("test user routes", () => {
 			};
 			let resp = await request(app).post("/users").send(v);
 			expect(resp.statusCode).toBe(201);
-			expect(resp.body).toEqual({ user: v });
+			v.password = expect.any(String);
+			expect(resp.body).toEqual({ token: expect.any(String), user: v });
 		});
 		test("provides an error response if username, password, first_name, last_name, email are not provided", async () => {
 			let resp = await request(app).post("/users");
@@ -99,7 +104,65 @@ describe("test user routes", () => {
 		});
 	});
 	describe("PATCH /users/:username", () => {
-		test("should update user details", async () => {
+		test("should update user details when JWT token username matches", async () => {
+			update = {
+				password: "updatedPW",
+				first_name: "updatedFirst",
+				last_name: "updatedLast",
+				email: "updatedTest@gmail.com",
+				photo_url: "",
+				is_admin: false,
+				_token: uToken,
+			};
+			u.first_name = "updatedFirst";
+			u.last_name = "updatedLast";
+			u.email = "updatedTest@gmail.com";
+			u.photo_url = "";
+			let resp = await request(app).patch(`/users/${u.username}`).send(update);
+			expect(resp.statusCode).toBe(200);
+			expect(resp.body).toEqual({ user: u });
+		});
+		test("provides an error response for incorrect paramter types when JWT token username matches", async () => {
+			v = {
+				username: 1,
+				password: 1,
+				first_name: 1,
+				last_name: 1,
+				email: 1,
+				photo_url: 1,
+				is_admin: 1,
+				_token: uToken,
+			};
+			let resp = await request(app).patch(`/users/${u.username}`).send(v);
+			expect(resp.statusCode).toBe(400);
+			expect(resp.body.message).toEqual([
+				"instance.password is not of a type(s) string",
+				"instance.first_name is not of a type(s) string",
+				"instance.last_name is not of a type(s) string",
+				"instance.email is not of a type(s) string",
+				"instance.photo_url is not of a type(s) string",
+				"instance.is_admin is not of a type(s) boolean",
+			]);
+		});
+		test("provides an error response for incorrect paramter types when JWT token username does not match", async () => {
+			update = {
+				password: "updatedPW",
+				first_name: "updatedFirst",
+				last_name: "updatedLast",
+				email: "updatedTest@gmail.com",
+				photo_url: "",
+				is_admin: false,
+				_token: "invalid",
+			};
+			u.first_name = "updatedFirst";
+			u.last_name = "updatedLast";
+			u.email = "updatedTest@gmail.com";
+			u.photo_url = "";
+			let resp = await request(app).patch(`/users/${u.username}`).send(update);
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
+		});
+		test("provides an error response for incorrect paramter types when JWT token username is not provided", async () => {
 			update = {
 				password: "updatedPW",
 				first_name: "updatedFirst",
@@ -113,41 +176,29 @@ describe("test user routes", () => {
 			u.email = "updatedTest@gmail.com";
 			u.photo_url = "";
 			let resp = await request(app).patch(`/users/${u.username}`).send(update);
-			expect(resp.statusCode).toBe(200);
-			expect(resp.body).toEqual({ user: u });
-		});
-		test("provides an error response for incorrect paramter types", async () => {
-			v = {
-				username: 1,
-				password: 1,
-				first_name: 1,
-				last_name: 1,
-				email: 1,
-				photo_url: 1,
-				is_admin: 1,
-			};
-			let resp = await request(app).patch(`/users/${u.username}`).send(v);
-			expect(resp.statusCode).toBe(400);
-			expect(resp.body.message).toEqual([
-				"instance.password is not of a type(s) string",
-				"instance.first_name is not of a type(s) string",
-				"instance.last_name is not of a type(s) string",
-				"instance.email is not of a type(s) string",
-				"instance.photo_url is not of a type(s) string",
-				"instance.is_admin is not of a type(s) boolean",
-			]);
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
 		});
 	});
 	describe("DELETE /user/:username", () => {
-		test("deletes a user based on the provided username", async () => {
-			let resp = await request(app).delete(`/users/${u.username}`);
+		test("deletes a user based on the provided username if JWT username matches", async () => {
+			let resp = await request(app).delete(`/users/${u.username}`).send({ _token: uToken });
 			expect(resp.statusCode).toBe(200);
 			expect(resp.body).toEqual({ message: "User deleted" });
 		});
-		test("provides an error response if the user can not be found", async () => {
-			let resp = await request(app).delete(`/users/invalid`);
+		test("provides an error response if the user can not be found if JWT username matches", async () => {
+			// first delete user
+			await User.remove(u.username);
+			let resp = await request(app).delete(`/users/${u.username}`).send({ _token: uToken });
 			expect(resp.statusCode).toBe(404);
-			expect(resp.body.message).toEqual(`Could not find user username: invalid`);
+			expect(resp.body.message).toEqual(`Could not find user username: ${u.username}`);
+		});
+		test("provides an error response if the user token does not match", async () => {
+			let resp = await request(app)
+				.delete(`/users/${u.username}`)
+				.send({ _token: "invalid" });
+			expect(resp.statusCode).toBe(401);
+			expect(resp.body.message).toEqual("Unauthorized");
 		});
 	});
 });
