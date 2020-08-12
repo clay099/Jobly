8b0cfd4e3fab43ee21291d4688ab4267c91d87ec
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-12 08:33:32 -0700

    added applications

--- data.sql
+++ data.sql
@@ -1,9 +1,12 @@
--- \c jobly
-\c jobly-test
+\c jobly
+-- \c jobly-test
 
 DROP TABLE IF EXISTS companies CASCADE;
 DROP TABLE IF EXISTS jobs CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS applications CASCADE;
+DROP TYPE IF EXISTS enum;
+
 
 CREATE TABLE companies (
     handle TEXT PRIMARY KEY,
@@ -31,4 +34,14 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     photo_url TEXT DEFAULT 'https://cdn3.vectorstock.com/i/1000x1000/21/62/human-icon-in-circle-vector-25482162.jp',
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TYPE enum AS ENUM ('interested', 'applied', 'accepted', 'rejected');
+
+CREATE TABLE applications (
+    username TEXT NOT NULL REFERENCES users ON DELETE CASCADE,
+    job_id INTEGER NOT NULL REFERENCES jobs ON DELETE CASCADE,
+    state enum,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT app_pk PRIMARY KEY(username,job_id)
 );
