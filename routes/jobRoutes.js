599795208bf9c65f8f4d5ff8212ae2ef0172f8c1
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-12 15:16:27 -0700

    updated apply status

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -132,7 +132,7 @@ router.post("/:id/apply", ensureLoggedIn, async (req, res, next) => {
 
 		// we know job passes and create in DB and return as json. Note there may be issue with PK username or PK job_id not being found
 		let application = await Application.create(obj);
-		return res.json({ message: application.state });
+		return res.status(201).json({ message: application.state });
 	} catch (e) {
 		return next(e);
 	}
