8c7ec58569708b0763b26c2ccbd4147bd263704c
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-18 09:49:15 -0700

    updated test db to be _

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const PORT = +process.env.PORT || 3000;
 // database is:
 //
 // - on Heroku, get from env var DATABASE_URL
-// - in testing, 'jobly-test'
+// - in testing, 'jobly_test'
 // - else: 'jobly'
 
 let DB_URI = `postgresql://`;
@@ -17,7 +17,7 @@ let BCRYPT_WORK_FACTOR;
 let JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "testKEY";
 
 if (process.env.NODE_ENV === "test") {
-	DB_URI = "jobly-test";
+	DB_URI = "jobly_test";
 	BCRYPT_WORK_FACTOR = 1;
 } else {
 	DB_URI = process.env.DATABASE_URL || "jobly";
